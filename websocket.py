0093a4b4eb6004dbf852ffc1350c02c8a0060d9d
Demo User <debian@beaglebone.localdomain>
2019-06-18 14:05:02 +0000

    modify websocket flow to have light function

--- websocket.py
+++ websocket.py
@@ -2,26 +2,48 @@ import ctypes
 import numpy as np
 import asyncio
 import websockets
+import time
 
 np.set_printoptions(suppress=True)
 
 m = ctypes.cdll.LoadLibrary('./lsm9ds0.so')
-# m.py_readAccel.restype = c_float
+gpio = ctypes.cdll.LoadLibrary('./gpio.so')
 
 m.py_readAccel.restype = np.ctypeslib.ndpointer(dtype=ctypes.c_float, shape=(6,))
+
+async def light():
+  print('light_on')
+  
+  gpio.open_pin()
+  print('turn_on')
+  gpio.turn_on()
+  await asyncio.sleep(0.5)
+  print('turn_off')
+  gpio.turn_off()
+  print('light_off')
+  #gpio.close_pin()
+
+
 
 async def hello(websocket, path):
   name = await websocket.recv()
 
   while(True):
       output = m.py_readAccel().tolist()
-      print(output)
-      print(type(output))
+      #print(output)
+      #print(type(output))
       obj = []
       for i in output:
           obj.append(str(i))
-      print(obj)
+      #print(obj)
       await websocket.send(obj[0])
+      #time.sleep(1)
+      
+      hit = await websocket.recv()
+      if hit == '1':
+        await light()
+
+
 
 start_server = websockets.serve(hello, '192.168.7.2', 8675)
 
